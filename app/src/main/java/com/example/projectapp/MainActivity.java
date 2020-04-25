1508a3fe447ccda61f963e9fb182cfbaec8bbc0d
ansharma03 <ansharma@bu.edu>
2020-04-25 16:15:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -0,0 +1,37 @@
+// ENTRY POINT OF THE APP --> DEFINES THE LAYOUT
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        //TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+    }
+
+//    called when user taps BEGIN button
+    public void Begin(View view) {
+        // go to next activity page
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
